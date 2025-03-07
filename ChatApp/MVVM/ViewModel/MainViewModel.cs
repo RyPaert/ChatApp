bbb54befd425a34c67684efdf37ae6f2fab6ad21
sdhsdh <sdhsdhsdh>
2025-03-07 10:39:00 +0200

    Add project files.

---
+++ ChatApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using ChatApp.MVVM.Core;
+using ChatApp.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApp.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        public string Username { get; set; }
+
+        private Server _server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer(Username), o => !string.IsNullOrEmpty(Username));
+        }
+    }
+}
---
+++ ChatServer/Client.cs
@@ -0,0 +1,30 @@
+﻿using ChatServer.Net.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    class Client
+    {
+        public string Username { get; set; }
+        public Guid UID { get; set; }
+        public TcpClient ClientSocket { get; set; }
+
+        PacketReaader _packetReader;
+
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+            _packetReader = new PacketReaader(ClientSocket.GetStream());
+            var opcode = _packetReader.ReadByte();
+            Username = _packetReader.ReadMessage();
+
+            Console.WriteLine($"{DateTime.Now}: Client has connected with username: {Username}");
+        }
+    }
+}
