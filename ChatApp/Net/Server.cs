5cb2b81032e002622e93f3b62b23b514a848f669
opilane <opilane@HP-CZC3217069>
2025-03-20 13:56:32 +0200

    started work on consoleapp

--- ChatApp/Net/Server.cs
+++ ChatApp/Net/Server.cs
@@ -26,7 +26,7 @@ namespace ChatApp.Net
         {
             if (!_client.Connected)
             {
-                _client.Connect("0.tcp.eu.ngrok.io", 14400);
+                _client.Connect("172.0.0.1", 14400);
                 PacketReader = new PacketReader(_client.GetStream());
 
                 if (!string.IsNullOrEmpty(username))
