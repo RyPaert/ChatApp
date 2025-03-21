c034fb9ea04ce9d249886a266151c1f9a9b70118
opilane <opilane@HP-CZC1339D29>
2025-03-21 10:56:02 +0200

    Can enter username, base program works, can send 1 message.

---
+++ ConsoleApp/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,56 @@
+﻿using ConsoleApp;
+using ConsoleApp.MVVM.Model;
+using System.Collections.ObjectModel;
+using System.Windows;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace ChatApp.MVVM.ViewModel
+{
+    class MainViewModel
+    {
+        public ObservableCollection<UserModel> Users { get; set; }
+        public ObservableCollection<string> Messages { get; set; }
+
+        public string Username { get; set; }
+        public string Message { get; set; }
+         
+        private Server _server;
+        public MainViewModel()
+        {
+            Users = new ObservableCollection<UserModel>();
+            Messages = new ObservableCollection<string>();
+
+            _server = new ConsoleApp.Server();
+            _server.connectedEvent += UserConnected;
+            _server.msgReceivedEvent += MessageReceived;
+            _server.userDisconnectEvent += RemoveUser;
+        }
+
+        private void RemoveUser()
+        {
+            var uid = _server.PacketReader.ReadMessage();
+            var user = Users.Where(x => x.UID == uid).FirstOrDefault();
+            Users.Remove(user);
+        }
+
+        private void MessageReceived()
+        {
+            var msg = _server.PacketReader.ReadMessage();
+            Messages.Add(msg);
+        }
+
+        private void UserConnected()
+        {
+            var user = new UserModel
+            {
+                Username = _server.PacketReader.ReadMessage(),
+                UID = _server.PacketReader.ReadMessage(),
+            };
+            if (!Users.Any( x => x.UID == user.UID))
+            {
+                Users.Add(user);
+            }
+        }
+    }
+}
